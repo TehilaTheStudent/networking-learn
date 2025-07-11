c41392f4687ca6aff29c53acc1f9384029aa429d
Tehila Aharonovich <146730488+TehilaTheStudent@users.noreply.github.com>
2025-07-11 16:10:58 +0000

    a

--- udemy/namespaces/many-ns.sh
+++ udemy/namespaces/many-ns.sh
@@ -47,8 +47,18 @@ ip netns exec blue ip route add 10.9.8.0/28 via 192.168.15.5
 ip netns exec red ip route
 ip netns exec blue ip route
 # if we ping now, we wont get a response,  sudo ip netns exec red ping 10.9.8.8
-# we need nat
-iptables -t nat -A POSTROUTING -s 192.168.15.0/24 -j MASQUERADE
+# we need nat, add rule in the nat ip table in the post routing chainto masquerade or replace th from address on all packets coming from the source network (192.168.1.0/24) with its own ip address
+# that way, anyone receiving these packets outside the network will think that they are coming from the host and not from within the namespaces
+# add a new 
+# iptables -t nat -A POSTROUTING -s 192.168.15.0/24 -j MASQUERADE
 
-sudo iptables -A FORWARD -i v-net-0 -j ACCEPT
-sudo iptables -A FORWARD -o v-net-0 -j ACCEPT
+# sudo iptables -A FORWARD -i v-net-0 -j ACCEPT
+# sudo iptables -A FORWARD -o v-net-0 -j ACCEPT
+
+# if we want to reach google from red and blue sudo ip netns exec blue ping 8.8.8.8 ping: connect: Network is unreachable
+# check route tables, ip route / route / route -n to disable names and show numbers
+# sudo ip netns exec red ip route add default via 192.168.15.5 ( the host )
+# now we can ping google from red and blue
+# how to reach namespace network from another host ? add a route tells: to reach this network go thgough the host 
+# add on host ip forwarding rule
+# sudo iptables -t nat -A PREROUTING ( -p tcp ) --dport 80 -j DNAT --to-destination 192.168.15.2:80 
